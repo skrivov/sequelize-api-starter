2cde47d1a9ab1b212420fecb6b1ba817145d6209
skrivov <skrivov@hotmail.com>
2019-12-02 22:51:59 -0500

    async tests

---
+++ src/__tests__/simple.js
@@ -0,0 +1,11 @@
+import 'regenerator-runtime/runtime'
+
+it('Testing to see if Jest works', () => {
+    expect(1).toBe(1)
+  })
+
+  it('Async test', async done => {
+    // Do your async tests here
+    
+    done()
+  })
